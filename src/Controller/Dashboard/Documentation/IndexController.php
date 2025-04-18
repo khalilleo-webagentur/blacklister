8d26572bcab7ba1dbc0495da58c4cc5eb15aa5be
khalilleo <dev@khalilleo.com>
2025-04-18 23:58:45 +0200

    dashboard documentation

---
+++ src/Controller/Dashboard/Documentation/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Dashboard\Documentation;
+
+use App\Controller\Dashboard\DashboardAbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('dashboard/i0n7v8d7o3m6')]
+class IndexController extends DashboardAbstractController
+{
+    #[Route('/doh5a6t0l1w2', name: 'app_dashboard_documentation_index')]
+    public function index(): Response
+    {
+        $this->hasRoleAdmin();
+
+        return $this->render('dashboard/documentation/index.html.twig');
+    }
+}
