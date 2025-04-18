7b5a7b6c9c0064017e0c9fd929b081f3a4f94ada
khalilleo <dev@khalilleo.com>
2025-04-18 23:58:07 +0200

    dashboard logs

---
+++ src/Controller/Dashboard/Logs/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Dashboard\Logs;
+
+use App\Controller\Dashboard\DashboardAbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('dashboard/i0n7v8d7o3m6')]
+class IndexController extends DashboardAbstractController
+{
+    #[Route('/loo5a6t0l1w2', name: 'app_dashboard_logs_index')]
+    public function index(): Response
+    {
+        $this->hasRoleAdmin();
+
+        return $this->render('dashboard/logs/index.html.twig');
+    }
+}
