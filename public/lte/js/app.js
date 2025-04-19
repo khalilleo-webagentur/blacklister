fa1abe2b90d2a8928723671ae9ae221b6d2cbe6a
khalilleo <dev@khalilleo.com>
2025-04-19 16:43:14 +0200

    dashboard copy API token to clipboard

--- public/lte/js/app.js
+++ public/lte/js/app.js
@@ -12,4 +12,31 @@ $(document).ready(function () {
             $('.ID').val($(this).attr('data-id'));
         });
     }
+
+    // copy to clipboard
+    let copyToClipBoard = $('.copyToClipBoard');
+    if (copyToClipBoard.length) {
+        copyToClipBoard.on('click', function (e) {
+            e.stopPropagation();
+            let content = $(this).attr('data-token');
+            if (content) {
+                copyContent(content);
+                alert('( ' + content + ') has been copied to clipboard.');
+            } else {
+                alert('cannot be copied to clipboard.');
+            }
+        });
+    }
 });
+
+function isLocalStorageAvailable() { return typeof (Storage) !== "undefined" }
+
+async function copyContent(text) {
+    try {
+        if (isLocalStorageAvailable) {
+            await navigator.clipboard.writeText(text);
+        }
+    } catch (err) {
+        console.error('clipboard is not available on your Browser.');
+    }
+}
